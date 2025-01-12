623dab07a7de204614ee833fe053beea3580c0b7
Altairie <arielwongyinhui@gmail.com>
2025-01-13 04:08:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,31 @@
 import {defineConfig, normalizePath} from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import path from 'path'
+<<<<<<< HEAD
 import topLevelAwait from 'vite-plugin-top-level-await'
+=======
+>>>>>>> parent of fb03b8b (Does JS work now)
 
 export default defineConfig({
     plugins: [
         viteStaticCopy({
+<<<<<<< HEAD
             targets: [topLevelAwait(),
+=======
+            targets: [
+>>>>>>> parent of fb03b8b (Does JS work now)
                 {
                     src: normalizePath(path.resolve(__dirname, './assets') + '/[!.]*'), // 1️⃣
                     dest: normalizePath('./assets'), // 2️⃣
                 },
             ],
         }),
+<<<<<<< HEAD
     ],
     base: "/IDD-project/"
+=======
+    ]
+>>>>>>> parent of fb03b8b (Does JS work now)
 })
 
 
