1fd160fca7ebd1d8599608bd7dcbafbd5ffc2bf2
Altairie <arielwongyinhui@gmail.com>
2025-01-13 03:45:18 +0800

    Test

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
-// import {defineConfig, normalizePath} from 'vite'
-// import { viteStaticCopy } from 'vite-plugin-static-copy'
-// import path from 'path'
-
-// export default defineConfig({
-//     plugins: [
-//         viteStaticCopy({
-//             targets: [
-//                 {
-//                     src: normalizePath(path.resolve(__dirname, './assets') + '/[!.]*'), // 1️⃣
-//                     dest: normalizePath('./assets'), // 2️⃣
-//                 },
-//             ],
-//         }),
-//     ]
-// })
-
-
-import { defineConfig } from 'vite'
+import {defineConfig, normalizePath} from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 import topLevelAwait from 'vite-plugin-top-level-await'
+
 export default defineConfig({
-plugins: [topLevelAwait()],
-base: './',
+    plugins: [
+        viteStaticCopy({
+            targets: [topLevelAwait(),
+                {
+                    src: normalizePath(path.resolve(__dirname, './assets') + '/[!.]*'), // 1️⃣
+                    dest: normalizePath('./assets'), // 2️⃣
+                },
+            ],
+        }),
+    ],
+    base: "/IDD-project/"
 })
+
+
